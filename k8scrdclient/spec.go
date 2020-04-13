5c571d5eb615e23d3e7c4bcf1b89d048b936cfec
Tim Schindler <xh3b4sd@gmail.com>
2020-04-13 17:05:23 +0200

    add crd client v1

--- k8scrdclient/spec.go
+++ k8scrdclient/spec.go
@@ -4,10 +4,13 @@ import (
 	"context"
 
 	"github.com/giantswarm/backoff"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 )
 
 type Interface interface {
 	EnsureCreated(ctx context.Context, customResource *apiextensionsv1beta1.CustomResourceDefinition, backOff backoff.Interface) error
+	EnsureCreatedV1(ctx context.Context, customResource *apiextensionsv1.CustomResourceDefinition, backOff backoff.Interface) error
 	EnsureDeleted(ctx context.Context, customResource *apiextensionsv1beta1.CustomResourceDefinition, backOff backoff.Interface) error
+	EnsureDeletedV1(ctx context.Context, customResource *apiextensionsv1.CustomResourceDefinition, backOff backoff.Interface) error
 }
