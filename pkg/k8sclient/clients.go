ac9c040117e5c78e49aad2f59dab12e9369f96ce
Jihyuk Bok <jhbok@giantswarm.io>
2020-08-06 13:50:30 +0200

    following changes to major version bump up

--- pkg/k8sclient/clients.go
+++ pkg/k8sclient/clients.go
@@ -1,7 +1,7 @@
 package k8sclient
 
 import (
-	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/apiextensions/v2/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -14,7 +14,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
-	"github.com/giantswarm/k8sclient/v3/pkg/k8scrdclient"
+	"github.com/giantswarm/k8sclient/v4/pkg/k8scrdclient"
 )
 
 type ClientsConfig struct {
