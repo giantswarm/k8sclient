e71d6742ed16361d0ea3a3df1a8c7f2b3e95b2cc
Nikola Prokopić <nikola@prokopic.rs>
2020-08-13 13:27:45 +0200

    Better error message in NewClients func

--- pkg/k8sclient/clients.go
+++ pkg/k8sclient/clients.go
@@ -48,7 +48,7 @@ func NewClients(config ClientsConfig) (*Clients, error) {
 	}
 
 	if config.KubeConfigPath == "" && config.RestConfig == nil {
-		return nil, microerror.Maskf(invalidConfigError, "%T.KubeConfigPath or %T.RestConfig must not be empty", config, config)
+		return nil, microerror.Maskf(invalidConfigError, "%T.KubeConfigPath or %T.RestConfig must not be empty (KubeConfigPath is empty: %t; RestConfig is empty: %t)", config, config, config.KubeConfigPath == "", config.RestConfig == nil)
 	}
 	if config.KubeConfigPath != "" && config.RestConfig != nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.KubeConfigPath and %T.RestConfig must not be set at the same time", config, config)
